7ff2f036725b6a3cf1faf4dbe6334eb428977eb6
codesyariah122 <pujiermanto@gmail.com>
2021-03-12 20:59:57 +0700

    update grid

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
   <div id="app">
     <Navigation />
-    <!--
-      NODE:
-      It's important to know that the router-view component here always renders the current route from your application. And that means in detail the component, which you have set in your router configuration. In this case, the article component. 
-
-      And this component is in this constellation also the child component at this time. And that's why we can use slots, and any other stuff we can do with a regular child component.
-    -->
+    
     <div class="content" :class="{'shrink':showNav}" @click="showNav = false">
       <router-view>
         <div id="navigation-icon">
-          <!--
-            By nested click events you need the stop modifer for avoiding the event propagation.
-          -->
+          
           <i class="fas fa-bars" @click.stop="showNav = true"></i>
         </div>
       </router-view>
